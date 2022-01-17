404c92971c0e33c78419e652a0d6a487d8512683
Jenya Poznyak <jendy.light@gmail.com>
2022-01-17 18:22:05 +0300

    lesson1

---
+++ src/search-form.ts
@@ -0,0 +1,76 @@
+import { renderBlock } from './lib.js';
+
+/** Функция рендера формы поиска
+ * @param date {Date} - дата 
+ * @param checkInDate {Date} - дата прибытия в отель
+ * @param checkOutDate {Date} - дата отбытия в отель
+ * @param daysToShift {number} - на какое количество дней сдвинуть
+
+ */
+
+const defaultDate = (date: Date, daysToShift: number): Date =>
+  new Date(date.getFullYear(), date.getMonth(), date.getDate() + daysToShift);
+
+const getLastDayOfNextMonth = (date: Date): Date =>
+  new Date(date.getFullYear(), date.getMonth() + 2, 0);
+
+const pad = (v: string | number): string =>
+`0${v}`.slice(-2);
+
+const formatDate = (date: Date): string =>
+  `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
+
+export function renderSearchFormBlock(
+  checkInDate?: Date,
+  checkOutDate?: Date
+): void {
+  // изменяем дату прибытия
+  checkInDate = checkInDate || defaultDate(new Date(), 1)
+  // форматируем дату
+  const checkIn = formatDate(checkInDate);
+  // изменяем дату отбытия
+  const checkOut = formatDate(checkOutDate || defaultDate(checkInDate, 2));
+  //форматируем текущую дату
+  const now = formatDate(new Date());
+  // получаем дату последнего дня в месяце
+  const lastDayOfNextMonth = formatDate(getLastDayOfNextMonth(new Date()));
+    
+  
+  renderBlock(
+    'search-form-block',
+    `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value="${checkIn}" min="${now}" max="${lastDayOfNextMonth}" name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value="${checkOut}" min="${checkIn}" max="${lastDayOfNextMonth}" name="checkout" />
+          </div>
+          <div>
+            <label for="max-price">Макс. цена суток</label>
+            <input id="max-price" type="text" value="" name="price" class="max-price" />
+          </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `
+  )
+}
---
+++ src/user.ts
@@ -0,0 +1,21 @@
+import { renderBlock } from './lib.js'
+
+export function renderUserBlock(favoriteItemsAmount: number, userName: string, userAvatar: string) {
+  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'; 
+  // const hasFavoriteItems = favoriteItemsAmount ? true : false
+  const hasFavoriteItems = !!favoriteItemsAmount;
+  renderBlock(
+    'user-block',
+    `
+    <div class="header-container">
+      <img class="avatar" src="${userAvatar}" alt="${userName}" />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  )
+}
