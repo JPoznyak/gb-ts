78787c6a9a4ae5c88dc5aff11e0415fb0e90e84b
Jenya Poznyak <jendy.light@gmail.com>
2022-01-27 18:25:23 +0300

    lesson4

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
-import { renderSearchFormBlock } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock, getUserData, getFavoritesAmount } from './user.js'
-import { renderToast } from './lib.js'
-import { getTodosByCount } from './todos.js'
+// import { renderSearchFormBlock } from './search-form.js'
+// import { renderSearchStubBlock } from './search-results.js'
+// import { renderUserBlock, getUserData, getFavoritesAmount } from './user.js'
+// import { renderToast } from './lib.js'
+// import { getTodosByCount } from './todos.js'
 
-window.addEventListener('DOMContentLoaded', () => {
-  // getUserData()
-  // getFavoritesAmount()
-  renderUserBlock('Wade Warren', '/img/avatar.png', 0)
-  renderSearchFormBlock()
-  renderSearchStubBlock()
-  getTodosByCount(10);
-  renderToast(
-      {
-        text: 'Это пример уведомления. Используйте его при необходимости', 
-        type: 'success'
-      },
-      {
-        name: 'Понял', 
-        handler: () => console.log('Уведомление закрыто')
-      }
-  )
-})
+// window.addEventListener('DOMContentLoaded', () => {
+//   // getUserData()
+//   // getFavoritesAmount()
+//   renderUserBlock('Wade Warren', '/img/avatar.png', 0)
+//   renderSearchFormBlock()
+//   renderSearchStubBlock()
+//   getTodosByCount(10);
+//   renderToast(
+//       {
+//         text: 'Это пример уведомления. Используйте его при необходимости', 
+//         type: 'success'
+//       },
+//       {
+//         name: 'Понял', 
+//         handler: () => console.log('Уведомление закрыто')
+//       }
+//   )
+// })
+
+import { } from './flat-rent-sdk.js' 
+
+
+
+
