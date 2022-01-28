1723e31c5b3e6490655d2655430a1c01057143d1
Jenya Poznyak <jendy.light@gmail.com>
2022-01-28 10:06:48 +0300

    d.js added

---
+++ src/flat-rent-sdk.d.ts
@@ -0,0 +1,7 @@
+// написать тайпинги для методов get, search, book
+export class FlatRentSdk {
+    get(id: string): {Promise<Object | null>}
+    search(parametrs: {Object}): {Object[]}
+    book(flatId: number, checkInDate: Date, checkOutDate: Date): number
+} 
+
