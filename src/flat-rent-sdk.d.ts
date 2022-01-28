4bd70d2121a82280a698de197700584f2a2db315
Jenya Poznyak <jendy.light@gmail.com>
2022-01-28 19:30:35 +0300

    fix

--- src/flat-rent-sdk.d.ts
+++ src/flat-rent-sdk.d.ts
@@ -1,7 +1,6 @@
 // написать тайпинги для методов get, search, book
 export class FlatRentSdk {
-    get(id: string): {Promise<Object | null>}
-    search(parametrs: {Object}): {Object[]}
+    get(id: string): Promise<Object | null>
+    search(parametrs: Object): Object[]
     book(flatId: number, checkInDate: Date, checkOutDate: Date): number
 } 
-
