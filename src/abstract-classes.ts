14adf05d71ef860a60c8377fb0fcc3bff80096ab
Jenya Poznyak <jendy.light@gmail.com>
2022-01-31 16:39:31 +0300

    lesson5

---
+++ src/abstract-classes.ts
@@ -0,0 +1,64 @@
+// Реализовать абстрактный класс MyGraphicsPrimitive2D 
+// у которого есть следующие свойства: 
+// прямоугольная область(2 точки, левая верхняя и правая нижняя), описывающая примитив; 
+// метод - переместить примитив на заданное смещение;. 
+// От него дожен наследоваться абстрактный класс MyAreaPrimitive2D, 
+// у которого есть метод площадь. От него должны наследоваться класс MyCircle, 
+// у него есть свойства: центр окружности и ее радиус, 
+// а также должен наследоваться класс MyRectangle с свойствами: ширина и высота
+
+abstract class MyGraphicsPrimitive2D {
+    protected leftTop: {x: number, y: number};
+    protected rightBottom: {x: number, y: number};
+    protected abstract movePrimitive(offsetX: number, offsetY: number): void;
+}
+
+abstract class MyAreaPrimitive2D extends MyGraphicsPrimitive2D {
+    protected abstract getArea(): void;
+} 
+
+export class MyCircle extends MyAreaPrimitive2D {
+    constructor(
+        public center: {x: number, y: number}, 
+        public radius: number,
+    ) {
+    super();
+  }
+  movePrimitive(offsetX: number, offsetY: number): void {
+    this.center.x += offsetX;
+    this.center.y += offsetY;
+  }
+  getArea(): void{
+      let pi = 3.14;
+      let circleArea = pi*this.radius*this.radius;
+      console.log("area =", circleArea);
+  }
+}
+
+export class MyRectangle extends MyAreaPrimitive2D {
+    constructor(
+        public width: number,
+        public height: number,
+        public leftTop: {x: number, y: number},
+        public rightBottom: {x: number, y: number},
+    ) {
+        super();
+    }
+    movePrimitive(offsetX: number, offsetY: number): void {
+    this.leftTop.x += offsetX;
+    this.leftTop.y += offsetY;
+    this.rightBottom.x += offsetX;
+    this.rightBottom.y += offsetY;
+  }
+  getArea(): void{
+      let square = this.width * this.height;
+      console.log("area =", square);
+  }
+}
+
+const circle = new MyCircle({x:1, y:3}, 5);
+circle.movePrimitive(2, 2);
+
+const rectangle = new MyRectangle(2, 5, {x:1, y:1}, {x:5, y:6});
+rectangle.movePrimitive(3, 3);
+
--- src/flat-rent-sdk.d.ts
+++ src/flat-rent-sdk.d.ts
@@ -1,6 +1,43 @@
-// написать тайпинги для методов get, search, book
+export interface Database {
+  id: string,
+  title: string,
+  details: string,
+  photos: string[],
+  coordinates: number[],
+  bookedDates: [],
+  price?: number,
+}
+
+export interface Parameters {
+  city: string,
+  checkInDate: Date,
+  checkOutDate: Date,
+  priceLimit: number
+}
+
+export function cloneDate(date: Date): Date
+
+export function addDays(date: Date, days: number): Date
+
+export const backendPort: number
+export const localStorageKey: string
+
 export class FlatRentSdk {
-    get(id: string): Promise<Object | null>
-    search(parametrs: Object): Object[]
-    book(flatId: number, checkInDate: Date, checkOutDate: Date): number
-} 
+  database: Database[]
+  constructor()
+
+  get(id: string): Promise<Database | null> 
+  search(parameters: Parameters): Promise<Database[]>
+  book(flatId: number, checkInDate: Date, checkOutDate: Date): Promise<number>
+  
+  _assertDatesAreCorrect(checkInDate: Date, checkOutDate: Date): void
+  _resetTime(date: Date): void
+  _calculateDifferenceInDays(startDate: Date, endDate: Date): number
+  _generateDateRange(from: Date, to: Date): Date[]
+  _generateTransactionId(): number
+  _areAllDatesAvailable(flat: Database, dateRange: Date[]): boolean
+  _formatFlatObject(flat: Database, nightNumber: number): Database
+  _readDatabase(): Database[] | null
+  _writeDatabase(database: Database[]): void
+  _syncDatabase(database: Database[]): void
+}
